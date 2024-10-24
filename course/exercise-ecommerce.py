4dafa6dd898b8c2f19de75d36c9ce9974b75d5e3
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2024-10-24 16:16:41 +0300

    exercise - ecommerce - basic solution

--- course/exercise-ecommerce.py
+++ course/exercise-ecommerce.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from time import time
 import os, sys
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
@@ -21,6 +23,39 @@ spark = SparkSession \
     Write the simplest code that does the job, then optimize it.
 """
 
+def basic():
+    orders_df = spark.read.option("header", "true").csv("../data/shopping/orders")
+    customers_df = spark.read.option("header", "true").csv("../data/shopping/customers")
+    products_df = spark.read.option("header", "true").csv("../data/shopping/products")
+    categories_df = spark.read.option("header", "true").csv("../data/shopping/categories")
+
+    revenue_by_customer = (
+        orders_df
+        .join(customers_df, 'customer_id')
+        .join(products_df, 'product_id')
+        .selectExpr('customer_id', 'order_amount * price as total_amount')
+        .groupBy('customer_id')
+        .agg(sum('total_amount').alias('total_revenue'))
+        .orderBy(col('total_revenue').desc())
+    )
+
+    revenue_by_category = (
+        orders_df
+        .join(products_df, 'product_id')
+        .join(categories_df, 'category_id')
+        .selectExpr('category_id', 'category_name', 'order_amount * price as total_amount')
+        .groupBy('category_id', 'category_name')
+        .agg(sum('total_amount').alias('total_revenue'))
+        .orderBy(col('total_revenue').desc())
+    )
+
+    start_time = time()
+    revenue_by_customer.show()
+    revenue_by_category.show()
+    print(f"Total time: {time() - start_time} seconds")
+    # 13s total (Daniel's machine)
+
+
 
 if __name__ == '__main__':
-    pass
+    basic()
